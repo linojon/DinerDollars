01fbc97acf724bdf4fc6f8eda8ab8fd9d74376ce
Jonathan Linowes <jonathan@parkerhill.com>
2014-09-24 22:25:37 -0400

    login and logout now working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140916191556) do
+ActiveRecord::Schema.define(version: 20140925021058) do
 
   create_table "enrollments", force: true do |t|
     t.integer  "shop_id"
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 20140916191556) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "type"
     t.string   "role"
   end
 
